98a0037f2b5bf437913f386e033bd6572c9cf3af
thisisaaronland <devnull@localhost>
2019-06-26 14:19:32 -0700

    start wiring up confirmation code

--- confirmations.go
+++ confirmations.go
@@ -6,8 +6,10 @@ import (
 	"github.com/aaronland/go-aws-session"
 	"github.com/aaronland/go-mailinglist/confirmation"
 	"github.com/aaronland/go-mailinglist/database"
+	aws "github.com/aws/aws-sdk-go/aws"
 	aws_session "github.com/aws/aws-sdk-go/aws/session"
 	aws_dynamodb "github.com/aws/aws-sdk-go/service/dynamodb"
+	aws_dynamodbattribute "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 const CONFIRMATIONS_DEFAULT_TABLENAME string = "confirmations"
@@ -68,15 +70,77 @@ func NewDynamoDBConfirmationsDatabaseWithSession(sess *aws_session.Session, opts
 }
 
 func (db *DynamoDBConfirmationsDatabase) AddConfirmation(conf *confirmation.Confirmation) error {
-	return errors.New("Please write me")
+
+	item, err := aws_dynamodbattribute.MarshalMap(conf)
+
+	if err != nil {
+		return err
+	}
+
+	req := &aws_dynamodb.PutItemInput{
+		Item:      item,
+		TableName: aws.String(db.options.TableName),
+	}
+
+	_, err = db.client.PutItem(req)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (db *DynamoDBConfirmationsDatabase) RemoveConfirmation(conf *confirmation.Confirmation) error {
-	return errors.New("Please write me")
+
+	req := &aws_dynamodb.DeleteItemInput{
+		TableName: aws.String(db.options.TableName),
+		Key: map[string]*aws_dynamodb.AttributeValue{
+			"code": {
+				S: aws.String(conf.Code),
+			},
+		},
+	}
+
+	_, err := db.client.DeleteItem(req)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (db *DynamoDBConfirmationsDatabase) GetConfirmationWithCode(code string) (*confirmation.Confirmation, error) {
-	return nil, errors.New("Please write me")
+
+	req := &aws_dynamodb.GetItemInput{
+		TableName: aws.String(db.options.TableName),
+		Key: map[string]*aws_dynamodb.AttributeValue{
+			"code": {
+				S: aws.String(code),
+			},
+		},
+	}
+
+	rsp, err := db.client.GetItem(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var conf *confirmation.Confirmation
+
+	err = aws_dynamodbattribute.UnmarshalMap(rsp.Item, &conf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if conf.Code == "" {
+		return nil, new(database.NoRecordError)
+	}
+
+	return conf, nil
 }
 
 func (db *DynamoDBConfirmationsDatabase) ListConfirmations(ctx context.Context, callback database.ListConfirmationsFunc) error {
